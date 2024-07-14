0bd2f69c0ddb551decafe64a6ff53cb3a4c0ec76
ImNotSarah <sarahdejesuslima09@gmail.com>
2024-07-13 22:40:31 -0300

    o que é programaçao orientada a objetos

---
+++ aula33.java
@@ -0,0 +1,71 @@
+/*
+
+1) Programação Linear
+
+permite ao usuário digitar numero 1
+permite ao usuário digitar numero 2
+
+somar numero 1 com numero 2
+
+exibir o resultado da soma
+
+
+2) Programação Estruturada
+
+funcao somar
+    permite ao usuário digitar numero 1
+    permite ao usuário digitar numero 2
+
+    somar numero 1 com numero 2
+    exibir o resultado da soma
+
+
+processo 01
+funcao somar
+processo 02
+funcao somar
+
+
+3) Programação Modular
+
+Módulo Usuario
+    funcao logar
+        recebe e-mail e senha
+        verifica se o usário existe
+        envia esse usuário a tela principal
+
+    funcao deslogar
+        sair do sistema
+        vai para tela inicial
+
+** Tela inicial do sistema
+Botão -> logar
+
+
+** Tela principal do sistema
+botao -> deslogar
+
+
+** tela de fornecedores
+deslogar
+logar
+
+3) Programação Orienta a objetos
+Classe fornecedor, cliente, produto...
+Classe Usuario
+    funcao logar
+        recebe e-mail e senha
+        verifica se o usário existe
+        envia esse usuário a tela principal
+
+    funcao deslogar
+        sair do sistema
+        vai para tela inicial
+
+* */
+
+class Scratch {
+    public static void main(String[] args) {
+        
+    }
+}
